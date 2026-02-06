835a60a97a946d4801c8168234ab4978b740c653
ld2208 <daveli@glint.com.hk>
2026-02-06 16:48:15 +0800

    Update __init__.py

--- source/magiclab_rl_lab/magiclab_rl_lab/tasks/__init__.py
+++ source/magiclab_rl_lab/magiclab_rl_lab/tasks/__init__.py
@@ -8,3 +8,18 @@ from isaaclab_tasks.utils import import_packages
 _BLACKLIST_PKGS = []
 # Import all configs in this package
 import_packages(__name__, _BLACKLIST_PKGS)
+
+import gymnasium as gym
+from .wiping.config.z1.wiping_env_cfg import Z1WipingEnvCfg
+
+gym.register(
+    id="Magiclab-Z1-12dof-Wiping",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "cfg": Z1WipingEnvCfg(),
+        "rsl_rl_cfg_entry_point": "magiclab_rl_lab.config.z1.agents.rsl_rl_cfg:Z1FlatPPOCfg",
+    },
+)
+
+
